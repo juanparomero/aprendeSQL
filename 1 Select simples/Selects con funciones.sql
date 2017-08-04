0a7b94a4f7f46ae99246c853ce0c3ad5dbb9143a
Juan Pablo Romero Aguirre <jp.romero@alumnos.upm.es>
2017-08-04 14:56:11 +0200

    Add numbers list

--- 1 Select simples/Selects con funciones.sql
+++ 1 Select simples/Selects con funciones.sql
@@ -2,6 +2,7 @@
 # SELECT CON FUNCIONES
 ------------------------------------------------------------------------------------------------
 /*
+| 1
 | Mostrar la fecha actual de la siguiente forma:
 | Fecha actual
 | ------------------------------
@@ -16,24 +17,30 @@ select rtrim(to_char(sysdate,'Day'))||', '||
     rtrim(to_char(sysdate,'month'))|| ' de '||
     to_char(sysdate,'yyyy')||'. '||to_char(sysdate,'hh24:mi:ss')  "Fecha actual"
 from dual;
+-- 2
 -- Dia en palabras en el cual naciste
 select rtrim(to_char(to_date('12/06/1987','dd/mm/yyyy'), 'Day')) "Dia de nacimiento"
 from dual;
+-- 3
 -- La suma de salarios, cuál es el mínimo, el máximo y la media de salario
 select sum(salary), min(salary), max(salary), avg(salary)
 from employees;
+-- 4
 -- Cuántos empleados hay, cuántos tienen salario y cuántos tienen comisión.
 select count(*), count(salary), count(commission_pct)
 from employees;
+-- 5
 -- Por un lado la media entre la media de salarios y el mínimo salario
 -- Y por otro lado, la media entre la media de salarios y el máximo salario
 -- Solo la parte entera, sin decimales ni redondeo.
 select trunc((avg(salary)+min(salary))/2) mediabaja, trunc((avg(salary)+max(salary))/2) mediaalta
 from employees;
+-- 6
 -- Listar el número de departamento y el máximo salario en cada uno de ellos.
 select department_id, max(salary)
 from employees
 group by department_id;
+-- 7
 -- Mostrar los nombres de los empleados que se repiten indicando cuántos hay del mismo
 -- en orden descendente.
 select first_name, count(first_name)
@@ -41,6 +48,7 @@ from employees
 group by first_name
 having count(first_name) != 1
 order by 2 desc;
+-- 8
 -- Mostrar en una sola consulta cuántos empleados son jefe de departamento
 -- y cuántos son jefes de empleados.
 select count( distinct manager_id) Id_Jefe, 'DEP' Jefe from departments
--- 1 Select simples/Selects simples.sql
+++ 1 Select simples/Selects simples.sql
@@ -1,42 +1,53 @@
 ------------------------------------------------------------------------------------------------
 -- SELECTS SIMPLES
 ------------------------------------------------------------------------------------------------
+-- 1
 -- Describir la tabla employees
 desc employees;
+-- 2
 -- Datos de la tabla regions
 select *
 from regions;
+-- 3
 -- Datos de la tabla countries
 select *
 from countries;
+-- 4
 -- Ciudad y estado de las localidades
 select city, state_province
 from locations;
+-- 5
 -- Número y nombre de departamento, además, el código del empleado jefe,
 -- de la localidad 1700.
 select department_id, department_name, manager_id
 from departments
 where location_id = 1700;
+-- 6
 -- Nombre y número de departamento de los empleados.
 select first_name, department_id
 from employees;
+-- 7
 -- Listar los distintos números de departamento en el que trabajan los empleados.
 select distinct department_id
 from employees;
+-- 8
 -- Nombre, apellido, salario ordenador por apellido asc y salario desc
 select first_name, last_name, salary
 from employees
 order by last_name asc, salary desc;
+-- 9
 -- códigos de los distintos trabajos que existen en el departamento 30
 select distinct job_id
 from employees
 where department_id = 30;
+-- 10
 -- Nombre, apellido y correo de los empleados del departamento 30
 -- cuyo salario es menor a 3000
 select first_name, last_name, email
 from employees
 where department_id = 30
     and salary < 3000;
+-- 11
 -- nombre, apellido y número de departamento de los empleados
 -- que no tengan comisión ordenados por número de departamento 
 -- del mayor a menor y por apellido.
@@ -44,17 +55,20 @@ select first_name, last_name, department_id
 from employees
 where commission_pct is not null
 order by 3 asc, 2 desc;
+-- 12
 -- Número de empleado, nombre y apellido de los empleados
 -- desde el apellido que empieza por L hasta los que su apellido
 -- empieza por la R, incluidos.
 select employee_id, first_name, last_name
 from employees
 where last_name between 'L' and 'R';
+-- 13
 -- Nombre de las columnas de la tabla de empleados 'Employees'
 select column_name
 from user_tab_columns
 where table_name = 'EMPLOYEES'
     and column_name not like '%@_%' escape '@';
+-- 14
 -- Nombre, apellido y el salario de los empleados
 -- pero como salario una etiqueta que indique 
 -- 'BAJO' si es menor a 4280, 'ALTO' si es mayor a 15230
